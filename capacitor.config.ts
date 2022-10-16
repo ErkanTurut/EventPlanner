f1f84306f5d03f716e4aa2f7a3d3b244e355f4fa
ErkanTurut <e.turut@students.ephec.be>
2022-10-16 14:10:45 +0200

    file name edited

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.testApp',
-  appName: 'testApp',
+  appId: 'com.EventPlanner',
+  appName: 'EventPlanner',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
